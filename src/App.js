3f6dceb977db96f18cfd1b4598eeeced671f8774
Eugene-prog <ieugene.prog@gmail.com>
2023-04-26 23:26:09 +0700

    feat: add useState setTodo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState(null);
+  console.log("App rerender");
+  
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => setTodo(json))
+ 
   return (
     <div className="App">
       
